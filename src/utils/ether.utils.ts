0753d49a11d341fe9849d60e3de22af99105c26d
storm0611 <devstar0611@gmail.com>
2024-05-13 05:38:26 +0200

    asdf

--- src/utils/ether.utils.ts
+++ src/utils/ether.utils.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers"
 
 export const generateNextHash = (from: string, nonce: number) => {
-    return ethers.utils.solidityKeccak256([ "string", "uint256" ], [from, nonce]);
+    return ethers.utils.solidityKeccak256([ "bytes32", "uint256" ], [from, nonce]);
 }
