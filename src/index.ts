d1891ccea4f25dcb8e5e4ebf83cc072e2218898e
storm0611 <devstar0611@gmail.com>
2024-05-13 07:06:47 +0200

    multiple nonces, verify

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import config from "./config";
-import { generateNextHash, generateRandomIntegerNumbers, loadDataFromJson, saveDataToJson } from "./utils";
+import { generateNextHash, generateNextHashes, generateRandomIntegerNumbers, loadDataFromJson, saveDataToJson, verify } from "./utils";
 
 const main = () => {
     const {min, max} = config;
     let from = config.initFrom;
     let count = config.count;
     const randNumbers = generateRandomIntegerNumbers(min, max, count);
-    let hashes = new Array<{hash: string, nonce: number | undefined}>();
+    const hashes = [];
     for (let i = 0; i < randNumbers.length; i++) {
-        const nonce = randNumbers[i];
+        const nonces = randNumbers.slice(i, i + 5);
         hashes.push({
             hash: from,
-            nonce: nonce
+            nonces: nonces
         })
-        from = generateNextHash(from, nonce);
+        from = generateNextHashes(from, nonces);
     }
     hashes.push({
         hash: from,
@@ -23,4 +23,5 @@ const main = () => {
     console.info(loadDataFromJson('out.json'))
 }
 
-main();
+// main();
+console.log(verify("0xf3e1230803372f3c957dc39d80a2977c9bd1fe9e4a4f8ece4a2c7a51a88c87ab", [ 45257, 23383, 55961, 85383, 79652 ], "0x5e4dc01ee91cfcb15411759234f37744a853e8ff3b98fa29cf76c001f4f6401d"))
--- src/utils/ether.utils.ts
+++ src/utils/ether.utils.ts
@@ -3,3 +3,19 @@ import { ethers } from "ethers"
 export const generateNextHash = (from: string, nonce: number) => {
     return ethers.utils.solidityKeccak256([ "bytes32", "uint256" ], [from, nonce]);
 }
+
+export const generateNextHashes = (from: string, nonces: Array<number>) => {
+    const types = [];
+    const args = [];
+    types.push("string");
+    args.push(from)
+    for (let i = 0; i < nonces.length; i++) {
+        types.push("uint256");
+        args.push(nonces[i]);
+    }
+    return ethers.utils.solidityKeccak256(types, args);
+}
+
+export const verify = (hash: string, nonces: Array<number>, nextHash: string) => {
+    return generateNextHashes(hash, nonces) == nextHash
+}
